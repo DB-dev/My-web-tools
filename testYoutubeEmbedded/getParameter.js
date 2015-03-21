fb647411588f2764f32282c67b5857525366ec9c
DB-dev <migueldcv.inf@gmail.com>
2015-03-21 20:15:31 +0100

    Youtube url embedder
    I had to create this little code for a webpage and decided to start
    using GitHub to store my little codes

---
+++ testYoutubeEmbedded/getParameter.js
@@ -0,0 +1,7 @@
+// Esta funcion extrae un parametro de una url dada 
+function getUrlParameter(url, name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(url);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
